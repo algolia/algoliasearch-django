80a355af0016ae9dc508769ce92157870f5bf5b1
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-06-12 15:52:06 +0200

    some clean

--- AlgoliaSearch/models.py
+++ AlgoliaSearch/models.py
@@ -1,5 +1,21 @@
-from django.db import models
+from __future__ import unicode_literals
 
 
-class SearchIndex(objet):
-    pass
+class AlgoliaIndex(objet):
+    '''An index in the Algolia backend.'''
+
+    # Use to specify the fields that should be included in the index.
+    fields = ()
+
+    # Use to specify the geo-fields that should be used for location search.
+    geo_fields = ()
+
+    # Use to specify the index to target on Algolia.
+    index_name = ()
+
+    # Use to specify the settings of the index.
+    settings = {}
+
+    def __init__(self, model):
+        '''Initializes the index.'''
+        self.model = model
--- AlgoliaSearch/registration.py
+++ AlgoliaSearch/registration.py
@@ -1,45 +1,12 @@
 from __future__ import unicode_literals
 
-import sys, json
-
 from django.conf import settings
-from django.core.signals import request_finished
-from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.db.models import Q
-from django.db.models.query import QuerySet
 from django.db.models.signals import post_save, pre_delete
-from django.utils.encoding import force_text
-from django.utils.html import strip_tags
-try:
-    from importlib import import_module
-except ImportError:
-    from django.utils.importlib import import_module
 
-from AlgoliaSearch.models import SearchIndex
+from AlgoliaSearch.models import AlgoliaIndex
 
-# TODO: clean import
-
-class IndexAdapterError(Exception):
-    '''Something went wrong with a index adapter.'''
-
-class IndexAdapter(object):
-    '''An adapater for indexing a model.'''
-
-    # Use to specify the fields that should be included in the index.
-    fields = ()
-
-    # Use to specify the geo-fields that should be used for location search.
-    geo_fields = ()
-
-    # Use to specify the settings of the index.
-    settings = ()
-
-    def __init__(self, model):
-        '''Initializes the index adapter.'''
-        self.model = model
-        # TODO: initializes other field
+from algoliasearch import algoliasearch
 
 
 class SearchEngineError(Exception):
@@ -57,7 +24,7 @@ class SearchEngine(object):
         '''Checks whether the given models is registered with the search engine.'''
         return model in self._registered_model
 
-    def register(self, model, adapter_cls=IndexAdapter):
+    def register(self, model, index_cls=AlgoliaIndex):
         '''
         Registers the given model with the search engine.
 
@@ -70,8 +37,8 @@ class SearchEngine(object):
                 model
             ))
         # Perform the registration.
-        adapter_obj = adapter_cls(model)
-        self._registered_models[model] = adapter_obj
+        index_obj = index_cls(model)
+        self._registered_models[model] = index_obj
         # Connect to the signalling framework.
         post_save.connect(self._post_save_receiver, model)
         pre_delete.connect(self._pre_delete_receiver, model)
