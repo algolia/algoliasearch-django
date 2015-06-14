a0a31fad674b4a04185d0feae613ae09b3d7b9e8
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-06-14 18:13:49 +0200

    better setup.py + add requirements.txt

--- setup.py
+++ setup.py
@@ -1,19 +1,37 @@
 import os
-from distutils.core import setup
+from setuptools import setup
 
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# Allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'algoliasearch-django',
     version = '1.0.0',
+    license = 'MIT License',
+    packages = ['AlgoliaSearch'],
+    install_requires = ['algoliasearch'],
     description = 'AlgoliaSearch integration for Django',
-    long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    author = 'Algolia',
+    long_description = README,
+    author = 'Algolia Team',
     author_email = 'hey@algolia.com',
     url = 'https://github.com/algolia/algoliasearch-django',
-    packages = [
-        'AlgoliaSearch',
-    ],
-    packages_dir = {
-        '': 'src'
-    }
+    keywords = ['algolia', 'pyalgolia', 'search', 'backend', 'hosted', 'cloud',
+        'full-text search', 'faceted search', 'django'],
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Interne :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Developement :: Libraries :: Python Modules'
+    ]
 )
