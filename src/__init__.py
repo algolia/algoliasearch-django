bcf6f6fb692e258df7e3cfbc0f20900504478347
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-07-10 10:13:05 +0200

    clean code

--- src/__init__.py
+++ src/__init__.py
@@ -5,13 +5,27 @@ http://www.algolia.com
 
 from __future__ import unicode_literals
 
-from django.contrib.algoliasearch.models import AlgoliaIndex
-from django.contrib.algoliasearch.registration import AlgoliaEngine
-from django.contrib.algoliasearch.registration import algolia_engine
+from . import models
+from . import registration
+from . import version
+from . import settings
 
-algolia_engine = algolia_engine
+
+ALGOLIA_VERSION = version.VERSION
+ALGOLIA_SETTINGS = settings.SETTINGS
+
+AlgoliaIndex = models.AlgoliaIndex
+AlgoliaEngine = registration.AlgoliaEngine
+algolia_engine = registration.algolia_engine
+
+
+# Algolia Engine functions
+
 register = algolia_engine.register
 unregister = algolia_engine.unregister
 get_registered_model = algolia_engine.get_registered_models
+
 get_adapter = algolia_engine.get_adapter
+get_adapter_from_instance = algolia_engine.get_adapter_from_instance
+
 raw_search = algolia_engine.raw_search
--- src/management/commands/algolia_reindex.py
+++ src/management/commands/algolia_reindex.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
-from django.contrib import algoliasearch
+
+from . import algoliasearch
 
 
 class Command(BaseCommand):
