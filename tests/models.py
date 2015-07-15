138cc68236b40c9ab422574cf85b204d9fce833c
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-07-15 12:35:34 +0200

    test commands with args

--- tests/models.py
+++ tests/models.py
@@ -11,3 +11,35 @@ class Example(models.Model):
 
     def location(self):
         return (self.lat, self.lng)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=140, blank=True)
+    followers_count = models.BigIntegerField(default=0)
+    following_count = models.BigIntegerField(default=0)
+    lat = models.FloatField(default=0)
+    lng = models.FloatField(default=0)
+
+    def location(self):
+        return (self.lat, self.lng)
+
+
+class Tweet(models.Model):
+    tweet_id = models.BigIntegerField(unique=True)
+    user = models.ForeignKey('User')
+    is_published = models.BooleanField(default=True)
+    published = models.DateTimeField(auto_now_add=True)
+    text = models.CharField(max_length=140)
+    retweet = models.PositiveIntegerField(default=0)
+    favori = models.PositiveIntegerField(default=0)
+    lat = models.FloatField(default=0)
+    lng = models.FloatField(default=0)
+
+    def location(self):
+        return (self.lat, self.lng)
+
+
+class Website(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField()
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,59 +1,127 @@
 from django.test import TestCase
 from django.core.management import call_command
 
-from algoliasearch_django import AlgoliaIndex, register, unregister
+from algoliasearch_django import register
+from algoliasearch_django import unregister
 
 try:
     from StringIO import StringIO
 except:
     from io import StringIO
 
-from .models import Example
+from .models import Website, User
 
 
 class CommandsTestCase(TestCase):
     def setUp(self):
-        class ExampleIndex(AlgoliaIndex):
-            field = 'name'
-
-        register(Example, ExampleIndex)
+        register(Website)
+        register(User)
         self.output = StringIO()
 
     def tearDown(self):
-        unregister(Example)
+        unregister(Website)
+        unregister(User)
 
     def test_reindex(self):
         call_command('algolia_reindex', stdout=self.output)
         self.output.seek(0)
-        result = self.output.readlines()
-        self.assertEqual(len(result), 2)
+        result = ' '.join(self.output.readlines())
 
-        regex = r'Example --> \d+'
+        regex = r'Website --> \d+'
         try:
-            self.assertRegex(result[1], regex)
+            self.assertRegex(result, regex)
         except AttributeError:
-            self.assertRegexpMatches(result[1], regex)
+            self.assertRegexpMatches(result, regex)
+
+        regex = r'User --> \d+'
+        try:
+            self.assertRegex(result, regex)
+        except AttributeError:
+            self.assertRegexpMatches(result, regex)
+
+    def test_reindex_with_args(self):
+        call_command('algolia_reindex', stdout=self.output, model=['Website'])
+        self.output.seek(0)
+        result = ' '.join(self.output.readlines())
+
+        regex = r'Website --> \d+'
+        try:
+            self.assertRegex(result, regex)
+        except AttributeError:
+            self.assertRegexpMatches(result, regex)
+
+        regex = r'User --> \d+'
+        try:
+            self.assertNotRegex(result, regex)
+        except AttributeError:
+            self.assertNotRegexpMatches(result, regex)
 
     def test_clearindex(self):
         call_command('algolia_clearindex', stdout=self.output)
         self.output.seek(0)
-        result = self.output.readlines()
-        self.assertEqual(len(result), 2)
+        result = ' '.join(self.output.readlines())
 
-        regex = r'Example'
+        regex = r'Website'
         try:
-            self.assertRegex(result[1], regex)
+            self.assertRegex(result, regex)
         except AttributeError:
-            self.assertRegexpMatches(result[1], regex)
+            self.assertRegexpMatches(result, regex)
+
+        regex = r'User'
+        try:
+            self.assertRegex(result, regex)
+        except AttributeError:
+            self.assertRegexpMatches(result, regex)
+
+    def test_clearindex_with_args(self):
+        call_command('algolia_clearindex', stdout=self.output,
+                     model=['Website'])
+        self.output.seek(0)
+        result = ' '.join(self.output.readlines())
+
+        regex = r'Website'
+        try:
+            self.assertRegex(result, regex)
+        except AttributeError:
+            self.assertRegexpMatches(result, regex)
+
+        regex = r'User'
+        try:
+            self.assertNotRegex(result, regex)
+        except AttributeError:
+            self.assertNotRegexpMatches(result, regex)
 
     def test_applysettings(self):
         call_command('algolia_applysettings', stdout=self.output)
         self.output.seek(0)
-        result = self.output.readlines()
-        self.assertEqual(len(result), 2)
+        result = ' '.join(self.output.readlines())
 
-        regex = r'Example'
+        regex = r'Website'
         try:
-            self.assertRegex(result[1], regex)
+            self.assertRegex(result, regex)
         except AttributeError:
-            self.assertRegexpMatches(result[1], regex)
+            self.assertRegexpMatches(result, regex)
+
+        regex = r'User'
+        try:
+            self.assertRegex(result, regex)
+        except AttributeError:
+            self.assertRegexpMatches(result, regex)
+
+    def test_applysettings_with_args(self):
+        call_command('algolia_applysettings', stdout=self.output,
+                     model=['Website'])
+        self.output.seek(0)
+        result = ' '.join(self.output.readlines())
+
+        regex = r'Website'
+        try:
+            self.assertRegex(result, regex)
+        except AttributeError:
+            self.assertRegexpMatches(result, regex)
+
+        regex = r'User'
+        try:
+            self.assertNotRegex(result, regex)
+        except AttributeError:
+            self.assertNotRegexpMatches(result, regex)
