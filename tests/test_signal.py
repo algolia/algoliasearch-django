bf1072d3902a61941189f091d571204bf62707de
Paul-Louis NECH <1821404+PLNech@users.noreply.github.com>
2018-05-15 12:47:21 +0000

    Fix/default log handler (#248)
    * chore: bump version to 1.5.5
    
    * fix(test_signal): Increase delay to 10s to avoid race condition, with FIXME
    
    * fix: Define default log handler

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -31,7 +31,7 @@ class SignalTestCase(TestCase):
         Website.objects.create(name='Google', url='https://www.google.com')
         Website.objects.create(name='Facebook', url='https://www.facebook.com')
 
-        time.sleep(5)
+        time.sleep(10)  # FIXME: Expose last task's ID so we can waitTask instead of sleeping
         self.assertEqual(raw_search(Website)['nbHits'], 3)
 
     def test_delete_signal(self):
@@ -42,7 +42,7 @@ class SignalTestCase(TestCase):
         Website.objects.get(name='Algolia').delete()
         Website.objects.get(name='Facebook').delete()
 
-        time.sleep(5)
+        time.sleep(10)
         result = raw_search(Website)
         self.assertEqual(result['nbHits'], 1)
         self.assertEqual(result['hits'][0]['name'], 'Google')
@@ -58,7 +58,7 @@ class SignalTestCase(TestCase):
         update_records(Website, qs, url='https://facebook.com')
         qs.update(url='https://facebook.com')
 
-        time.sleep(5)
+        time.sleep(10)
         result = raw_search(Website, 'Facebook')
         self.assertEqual(result['nbHits'], qs.count())
         for res, url in zip(result['hits'], qs.values_list('url', flat=True)):
