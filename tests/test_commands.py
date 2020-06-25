60adfcb5916e297ebaa904a9db3737582cd779f7
Dan Wilson <dan.wilson@brightnetwork.co.uk>
2020-06-25 10:23:04 +0100

    fix: Rewrite management command tests
    Rather than testing what's printed to stdout, these use mocks to
    make sure the correct methods are being called.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,133 +1,66 @@
+from mock import patch, call, Mock
+
 from django.test import TestCase
 from django.utils.six import StringIO
 from django.core.management import call_command
-
-from algoliasearch_django import algolia_engine
-from algoliasearch_django import get_adapter
-from algoliasearch_django import clear_index
 
 from .models import Website
 from .models import User
 
 
 class CommandsTestCase(TestCase):
-    @classmethod
-    def tearDownClass(cls):
-        algolia_engine.client.delete_index(get_adapter(User).index_name)
-        algolia_engine.client.delete_index(get_adapter(Website).index_name)
-
-    def setUp(self):
-        # Create some records
-        User.objects.create(name='James Bond', username="jb")
-        User.objects.create(name='Captain America', username="captain")
-        User.objects.create(name='John Snow', username="john_snow",
-                            _lat=120.2, _lng=42.1)
-        User.objects.create(name='Steve Jobs', username="genius",
-                            followers_count=331213)
-
-        self.out = StringIO()
-
-    def tearDown(self):
-        clear_index(Website)
-        clear_index(User)
 
     def test_reindex(self):
-        call_command('algolia_reindex', stdout=self.out)
-        result = self.out.getvalue()
-
-        regex = r'Website --> 0'
-        try:
-            self.assertRegex(result, regex)
-        except AttributeError:
-            self.assertRegexpMatches(result, regex)
-
-        regex = r'User --> 4'
-        try:
-            self.assertRegex(result, regex)
-        except AttributeError:
-            self.assertRegexpMatches(result, regex)
+        with patch("algoliasearch_django.management.commands.algolia_reindex.reindex_all") as mock_reindex:
+            call_command('algolia_reindex', stdout=StringIO())
+        mock_reindex.assert_has_calls([
+            call(Website, batch_size=1000),
+            call(User, batch_size=1000)
+        ], any_order=True)
 
     def test_reindex_with_args(self):
-        call_command('algolia_reindex', stdout=self.out, model=['Website'])
-        result = self.out.getvalue()
-
-        regex = r'Website --> \d+'
-        try:
-            self.assertRegex(result, regex)
-        except AttributeError:
-            self.assertRegexpMatches(result, regex)
-
-        regex = r'User --> \d+'
-        try:
-            self.assertNotRegex(result, regex)
-        except AttributeError:
-            self.assertNotRegexpMatches(result, regex)
+        with patch("algoliasearch_django.management.commands.algolia_reindex.reindex_all") as mock_reindex:
+            call_command('algolia_reindex', stdout=StringIO(), model=['Website'])
+        mock_reindex.assert_called_once_with(Website, batch_size=1000)
 
     def test_clearindex(self):
-        call_command('algolia_clearindex', stdout=self.out)
-        result = self.out.getvalue()
-
-        regex = r'Website'
-        try:
-            self.assertRegex(result, regex)
-        except AttributeError:
-            self.assertRegexpMatches(result, regex)
-
-        regex = r'User'
-        try:
-            self.assertRegex(result, regex)
-        except AttributeError:
-            self.assertRegexpMatches(result, regex)
+        with patch("algoliasearch_django.management.commands.algolia_clearindex.clear_index") as mock_clear:
+            call_command('algolia_clearindex', stdout=StringIO())
+        mock_clear.assert_has_calls([
+            call(Website),
+            call(User)
+        ], any_order=True)
 
     def test_clearindex_with_args(self):
-        call_command(
-            'algolia_clearindex',
-            stdout=self.out,
-            model=['Website']
-        )
-        result = self.out.getvalue()
-
-        regex = r'Website'
-        try:
-            self.assertRegex(result, regex)
-        except AttributeError:
-            self.assertRegexpMatches(result, regex)
-
-        regex = r'User'
-        try:
-            self.assertNotRegex(result, regex)
-        except AttributeError:
-            self.assertNotRegexpMatches(result, regex)
+        with patch("algoliasearch_django.management.commands.algolia_clearindex.clear_index") as mock_clear:
+            call_command(
+                'algolia_clearindex',
+                stdout=StringIO(),
+                model=['Website']
+            )
+        mock_clear.assert_called_once_with(Website)
 
     def test_applysettings(self):
-        call_command('algolia_applysettings', stdout=self.out)
-        result = self.out.getvalue()
+        with patch("algoliasearch_django.management.commands.algolia_applysettings.get_adapter") as mock_get_adapter:
+            mock_adapter = Mock()
+            mock_get_adapter.return_value = mock_adapter
+            call_command('algolia_applysettings', stdout=StringIO())
 
-        regex = r'Website'
-        try:
-            self.assertRegex(result, regex)
-        except AttributeError:
-            self.assertRegexpMatches(result, regex)
+        mock_get_adapter.assert_has_calls([
+            call(Website),
+            call(User)
+        ], any_order=True)
 
-        regex = r'User'
-        try:
-            self.assertRegex(result, regex)
-        except AttributeError:
-            self.assertRegexpMatches(result, regex)
+        mock_adapter.set_settings.assert_has_calls([
+            call(),
+            call()
+        ])
 
     def test_applysettings_with_args(self):
-        call_command('algolia_applysettings', stdout=self.out,
-                     model=['Website'])
-        result = self.out.getvalue()
+        with patch("algoliasearch_django.management.commands.algolia_applysettings.get_adapter") as mock_get_adapter:
+            mock_adapter = Mock()
+            mock_get_adapter.return_value = mock_adapter
+            call_command('algolia_applysettings', stdout=StringIO(), model=['Website'])
 
-        regex = r'Website'
-        try:
-            self.assertRegex(result, regex)
-        except AttributeError:
-            self.assertRegexpMatches(result, regex)
-
-        regex = r'User'
-        try:
-            self.assertNotRegex(result, regex)
-        except AttributeError:
-            self.assertNotRegexpMatches(result, regex)
+        mock_get_adapter.assert_called_once_with(Website)
+        mock_adapter.set_settings.assert_called_once()
