41f09248a8455c1bb807e1803893fb4cb6abe49b
Dan Wilson <dan.wilson@brightnetwork.co.uk>
2020-07-27 09:54:03 +0100

    fix: Publish README using pypi markdown support

--- setup.py
+++ setup.py
@@ -11,12 +11,8 @@ from setuptools import find_packages
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 path_readme = os.path.join(os.path.dirname(__file__), 'README.md')
-try:
-    import pypandoc
-    README = pypandoc.convert(path_readme, 'rst')
-except (IOError, ImportError):
-    with open(path_readme) as readme:
-        README = readme.read()
+with open(path_readme) as readme:
+    README = readme.read()
 
 path_version = os.path.join(os.path.dirname(__file__),
                             'algoliasearch_django/version.py')
@@ -31,6 +27,7 @@ setup(
     install_requires=['django>=2.2', 'algoliasearch>=1.0,<2.0'],
     description='Algolia Search integration for Django',
     long_description=README,
+    long_description_content_type="text/markdown",
     author='Algolia Team',
     author_email='support@algolia.com',
     url='https://github.com/algolia/algoliasearch-django',
