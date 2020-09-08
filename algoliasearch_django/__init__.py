c6bf12af71002f8078f2aadd072723a0dbd958e6
Dan Wilson <dan.wilson@brightnetwork.co.uk>
2020-09-08 10:28:51 +0100

    fix: Expose get_registered_models without a typo :)
    The original name can be removed at next minor release

--- algoliasearch_django/__init__.py
+++ algoliasearch_django/__init__.py
@@ -24,7 +24,8 @@ algolia_engine = registration.algolia_engine
 register = algolia_engine.register
 register_aggregator = algolia_engine.register_aggregator
 unregister = algolia_engine.unregister
-get_registered_model = algolia_engine.get_registered_models
+get_registered_model = algolia_engine.get_registered_models  # Deprecated
+get_registered_models = algolia_engine.get_registered_models
 
 get_registered_adapters = algolia_engine.get_registered_adapters
 get_adapter = algolia_engine.get_adapter
